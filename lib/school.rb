abeed2dd942dcb0f3c5381f66c52770d05cc6e01
Shannon Young <shannon.ari1@gmail.com>
2021-04-27 18:34:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,22 +1,24 @@
 # code here!
 class School
     attr_accessor :name, :roster
-    def initialize(name)
+    def initialize(name) #initialized with a name
         @name=name
-        @roster={}
+        @roster={} #empty hash upon initialization 
     end
 
-    def add_student(name, grade)
-
+    def add_student(name, grade) #method with an argument of the student's name
+                                 #and their grade
         @roster[grade] ||= []
         @roster[grade] << name
     end
 
-    def grade(grade)
+    def grade(grade) #Method that takes in an argument of a grade and
+                     #returns all of the students in that grade.
         @roster[grade]
     end
 
-    def sort
+    def sort #sorted list of all the students where the strings 
+            #in the student arrays are sorted alphabetically.
         sorted_hash = {}
         @roster.each do |grade_key, student_array|
             sorted_hash[grade_key] = student_array.sort
